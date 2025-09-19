a379af01bc629d1b5bd8376949513e7c6c203026
Débora Nogueira <debora.nogueira@sou.inteli.edu.br>
2025-09-19 15:44:07 -0300

    second line of best fit

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -15,28 +15,46 @@ def draw_plot():
 
     plt.scatter(years, seaLevel)
 
-# Create first line of best fit // linha que representa a tendência
+# Create first line of best fit
 
     # Definição do INTERVALO de anos.
-    x_pred =  pd.Series(range(1880, 2051)) # o range gera a sequência de anos, e o series transforma os dados em objeto pandas que pode ser usado para operações matemáticas
+    x_1st =  pd.Series(range(1880, 2051)) # o range gera a sequência de anos, e o series transforma os dados em objeto pandas que pode ser usado para operações matemáticas
 
     # Cálculo dos PARÂMETROS que serão utilizados na regressão linear
     slope, intercept, *_ = linregress(years, seaLevel) # atribuição de multiplos valores de uma só vez com tupla, ignorando os ultimos 3 valores que são retornados com a função
 
     # Escrevendo a regressão linear que vai gerar as PREVISÕES, com a equação matemática da reta: y = m*x + b (m -> slope (coeficiente angular); b -> intercept (cruzamento no Y))
-    y_pred = (slope * x_pred) + intercept
+    y_pred = (slope * x_1st) + intercept
 
     # Plotando a linha vermelha no scatterplot já existente
-    plt.plot(x_pred, y_pred, color='red')
+    plt.plot(x_1st, y_pred, color='red')
 
 # Create second line of best fit
+
+    # Novo intervalo de dados
+    df_recent = df[df['Year'] >= 2000]
+
+    years_recent = df_recent["Year"]
+    seaLevel_recent = df_recent['CSIRO Adjusted Sea Level']
+
+    # Definição do novo INTERVALO de anos.
+    x_2nd =  pd.Series(range(2000, 2051))
+
+    # Novos parâmetros
+    slope_recent, intercept_recent, *_ = linregress(years_recent, seaLevel_recent)
+
+    # Regressão linear
+    y_pred2 = (slope_recent * x_2nd) + intercept_recent
+
+    # Plotando a linha roxa no scatterplot
+    plt.plot(x_2nd, y_pred2, color='purple')
 
 
 # Add labels and title
 
-    plt.title('Sea-Level Scatterplot')
+    plt.title('Rise in Sea Level')
     plt.xlabel('Year')
-    plt.ylabel('CSIRO Sea Level (inches)')
+    plt.ylabel('Sea Level (inches)')
 
 # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
